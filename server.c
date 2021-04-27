10d0fcd16e00c4529d43789fe6e91d4bcd5c665f
Aromanushko <79808673+Aromanushko@users.noreply.github.com>
2021-04-26 21:43:29 -0400

    Add files via upload
    Implemented socket example from class into code. 
    Implemented framework for connection Handler.

--- server.c
+++ server.c
@@ -1,3 +1,4 @@
+#define _POSIX_C_SOURCE 200809L
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -14,7 +15,13 @@
 #include <ctype.h>
 #include <limits.h>
 
+// define amount of connections to put into backlog
+#define BACKLOG 5
 
+// server is running by default
+int running = 1;
+
+// node for use in hash table
 typedef struct node{
     char* key;
     char* value;
@@ -178,86 +185,236 @@ char* delete(char* key){
 	return prev_val;
 }
 
+// struct for use in connection threads
+struct connection {
+    struct sockaddr_storage addr;
+    socklen_t addr_len;
+    int fd;
+};
+// method for connection handler
+void *connectionHandler(void *arg);
 //A key-value storage service
 int main(int argc, char** argv){
     if(argc != 2){
         return EXIT_FAILURE;
     }
-    int port = atoi(argv[1]);
-    if(port == 0){
-        return EXIT_FAILURE;
-    }
-
+    char* port = argv[1];
+    /*
     node root;
     root.key = malloc(sizeof(char)*100);
     root.value = malloc(sizeof(char)*100);
     root.next = malloc(sizeof(node));
-    root.key = "day";
-    root.value = "Monday";
-    root.next->key = "month";
-    root.next->value = "February";
-    hashtable[hashCode("day")] = &root;
-    insert("season", "Summer");
- 
-    node* traverse = &root;
-    while(traverse!=NULL){
-            printf("%s\n", traverse->key);
-	    
-            traverse = traverse->next;
-    }
-    node* temp = search("day");
-    printf("Value of day key is: %s\n", temp->value);
-    node* tem = search("season");
-    if(tem == NULL){
-	printf("Insert not working correctly\n");
-    }
-    else{
-   	 printf("Value of season key is: %s\n", tem->value);
-    }
+    */
     //open and bind a listening socket on specified port and wait for incoming connection requests
-    int server_fd, new_socket, valread;
-    struct sockaddr_in address;
-    int addrlen = sizeof(address);
-    int opt = 1;
-    char buffer[1024] = {0};
-    char* hello = "Hello from server";
-    //Create socket
-    if((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0){
-		perror("socket failed");
-		return(EXIT_FAILURE);
+    struct addrinfo hint, *info_list, *info;
+    struct connection *con;
+    int error, sfd;
+    pthread_t tid;
+
+    // initialize hints
+    memset(&hint, 0, sizeof(struct addrinfo));
+    hint.ai_family = AF_UNSPEC;
+    hint.ai_socktype = SOCK_STREAM;
+    hint.ai_flags = AI_PASSIVE;
+    // setting AI_PASSIVE means that we want to create a listening socket
+    // get socket and address info for listening port
+    // - for a listening socket, give NULL as the host name (because the socket is on
+    //   the local host)
+    error = getaddrinfo(NULL, port, &hint, &info_list);
+    if (error != 0) {
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(error));
+        return -1;
     }
-    //Open socket to port
-    if(setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))){
-		perror("socket failed");
-		return(EXIT_FAILURE);
-   }
-   address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons( port );
-       
-    // bind socket to specified port
-    if (bind(server_fd, (struct sockaddr *)&address, 
-                                 sizeof(address))<0)
+    // attempt to create socket
+    for (info = info_list; info != NULL; info = info->ai_next) {
+        sfd = socket(info->ai_family, info->ai_socktype, info->ai_protocol);
+        
+        // if we couldn't create the socket, try the next method
+        if (sfd == -1) {
+            continue;
+        }
+
+        // if we were able to create the socket, try to set it up for
+        // incoming connections;
+        // 
+        // note that this requires two steps:
+        // - bind associates the socket with the specified port on the local host
+        // - listen sets up a queue for incoming connections and allows us to use accept
+        if ((bind(sfd, info->ai_addr, info->ai_addrlen) == 0) &&
+            (listen(sfd, BACKLOG) == 0)) {
+            break;
+        }
+
+        // unable to set it up, so try the next method
+        close(sfd);
+    }
+
+    if (info == NULL) {
+        // we reached the end of result without successfuly binding a socket
+        fprintf(stderr, "Could not bind\n");
+        return -1;
+    }
+
+    freeaddrinfo(info_list);
+
+	struct sigaction act;
+	act.sa_handler = handler;
+	act.sa_flags = 0;
+	sigemptyset(&act.sa_mask);
+	sigaction(SIGINT, &act, NULL);
+	
+	sigset_t mask;
+	
+	sigemptyset(&mask);
+	sigaddset(&mask, SIGINT);
+
+
+    // at this point sfd is bound and listening
+    printf("Waiting for connection\n");
+	while (running) {
+    	// create argument struct for child thread
+		con = malloc(sizeof(struct connection));
+        con->addr_len = sizeof(struct sockaddr_storage);
+        	// addr_len is a read/write parameter to accept
+        	// we set the initial value, saying how much space is available
+        	// after the call to accept, this field will contain the actual address length
+        
+        // wait for an incoming connection
+        con->fd = accept(sfd, (struct sockaddr *) &con->addr, &con->addr_len);
+        	// we provide
+        	// sfd - the listening socket
+        	// &con->addr - a location to write the address of the remote host
+        	// &con->addr_len - a location to write the length of the address
+        	//
+        	// accept will block until a remote host tries to connect
+        	// it returns a new socket that can be used to communicate with the remote
+        	// host, and writes the address of the remote hist into the provided location
+        
+        // if we got back -1, it means something went wrong
+        if (con->fd == -1) {
+            perror("accept");
+            continue;
+        }
+        
+        // temporarily block SIGINT (child will inherit mask)
+        error = pthread_sigmask(SIG_BLOCK, &mask, NULL);
+        if (error != 0) {
+        	fprintf(stderr, "sigmask: %s\n", strerror(error));
+        	abort();
+        }
+
+		// spin off a worker thread to handle the remote connection
+        error = pthread_create(&tid, NULL, connectionHandler, con);
+
+		// if we couldn't spin off the thread, clean up and wait for another connection
+        if (error != 0) {
+            fprintf(stderr, "Unable to create thread: %d\n", error);
+            close(con->fd);
+            free(con);
+            continue;
+        }
+
+		// otherwise, detach the thread and wait for the next connection request
+        pthread_detach(tid);
+
+        // unblock SIGINT
+        error = pthread_sigmask(SIG_UNBLOCK, &mask, NULL);
+        if (error != 0) {
+        	fprintf(stderr, "sigmask: %s\n", strerror(error));
+        	abort();
+        }
+
+    }
+
+	puts("No longer listening.");
+	pthread_detach(pthread_self());
+	pthread_exit(NULL);
+
+    // never reach here
+    return 0;
+}
+
+void *connectionHandler(void *arg)
+{
+    
+    struct connection *c = (struct connection *) arg;
+    int error, nread;
+    // input array which will contain, at most, 4 parameters TODO : function to parse input from client into this format
+    // input[0] - command
+    // input[1] - msg length
+    // input[2] - key
+    // input[3] - value, should be set to an empty string if command is not SET
+    char* input[4];
+    // msg returned by handler to client, indicates if operation was succesful or not
+    char* retMsg;
+    // error flag, 0 means no error, 1 means error, used when determining if the loop should continue
+    int error = 0;
+    // temp char* to store messages retrieved from hash table, returned to client to display hash table contents when GET is used
+    char* tempMsg;
+    // length of key and message combined
+    int rLen = strlen(input[2]) + strlen(input[3]);
+    // TODO : locks must be implemented to ensure that all behavior is deterministic when writing to/ reading from hash table
+    // also retMsg needs to be returned to client through a helper method that formats it appropriately 
+
+    // while no errors have occured, process commands from client
+    while(error == 0)
     {
-        perror("bind failed");
-        exit(EXIT_FAILURE);
+        // if-else tree to parse commands, should restart after every succesful set of commands/parameters
+        // check to see if length is correct, if not, set an error message
+        if(rLen == atoi(input[1]))
+        {
+            // attempt to run GET command, return OKG on success
+            if(strcmp(input[0], "GET") == 0)
+            {
+                // if key is in hash table, get the value associated, else return error
+                // TODO return tempMsg to client in similar way to retMSG
+                if(search(input[2]))
+                {
+                    tempMsg = search(input[2])->value;
+                    // msg indicates operation success
+                    retMsg = "OKG";
+                }
+                // key not found, set return message to notify user, but do not cause error and end loop
+                else
+                {
+                    retMsg = "KNF"; 
+                }
+            }
+            // attempt to run SET command, return OKS on success
+            else if(strcmp(input[0], "SET") == 0)
+            {
+                insert(input[2],input[3]);
+                retMsg = "OKS";
+            }
+            // attempt to run DEL command, return OKD on success
+            else if(strcmp(input[0], "DEL") == 0)
+            {
+                if(delete(input[2]))
+                {
+                    retMsg = "OKD";
+                }
+                else
+                {
+                    // key not found, set return message to notify user, but do not cause error and end loop
+                    retMsg = "KNF";
+                }
+            }
+            else
+            {
+                // Message is malformed
+                retMsg = "ERR\nBAD\n";
+                error = 1;
+            }
+        }
+        else
+        {
+            // Message length is inconsistent with actual length
+            retMsg = "ERR\nLEN\n";
+            error = 1;
+        }
     }
-    if (listen(server_fd, 3) < 0)
-    {
-        perror("listen");
-        exit(EXIT_FAILURE);
-    }
-    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, 
-                       (socklen_t*)&addrlen))<0)
-    {
-        perror("accept");
-        exit(EXIT_FAILURE);
-    }
-    valread = read( new_socket , buffer, 1024);
-    printf("%s\n",buffer );
-    send(new_socket , hello , strlen(hello) , 0 );
-   
-    printf("Port number to listen on: %d\n", port);
-    printf("Hi from server (:\n");
-	return 0;
+    // close and free connection thread
+    close(c->fd);
+    free(c);
+    return NULL;
 }
